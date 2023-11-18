ade3bff5650ed043ee834d2dcafc42051dded2b5
Victoria Diaz Diaz <vdiaz043@xe-02.cs.ucr.edu>
2023-11-18 12:45:09 -0800

    Part 3

---
+++ main.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main () {
+//    double Exchange;
+//    string ISOInput;
+//    double inputUSD= 1.00;
+
+//    cout << "enter ISOInput" <<endl;
+//    cin >> ISOInput;
+
+
+   if (ISOInput == "EUR") {
+    Exchange=  inputUSD * 0.92;
+  }
+  else if ( ISOInput =="FJD") {
+    Exchange = inputUSD * 2.29;
+  }
+   else if ( ISOInput =="FKP" ){
+    Exchange = inputUSD * 0.80;
+  }
+   else if ( ISOInput =="FOK") {
+    Exchange = inputUSD *0.0;    // couldn't find
+  }
+   else if ( ISOInput =="GBP") {
+    Exchange = inputUSD * 0.80;
+  }
+   else if ( ISOInput =="GEL") {
+    Exchange = inputUSD * 2.70;
+  }
+   else if ( ISOInput =="GGP") {
+    Exchange = inputUSD * 0.80;  //verify again
+  }
+  else if ( ISOInput == "GHS"){
+    Exchange= inputUSD * 11.90;
+
+  }
+  else if ( ISOInput == "GIP"){
+    Exchange = inputUSD * 0.806;
+  }
+  else if (ISOInput == "GMD"){
+    Exchange = inputUSD * 67.25;
+  }
+  else if ( ISOInput == "GNF"){
+    Exchange = inputUSD * 8550.76;  //bigger currency
+  }
+  else if ( ISOInput == "GTQ"){
+    Exchange = inputUSD * 7.79;
+  }
+  else if (ISOInput == "GYD"){
+    Exchange = inputUSD * 208.20;
+  }
+  else if ( ISOInput == "HKD"){
+    Exchange = inputUSD * 7.80;
+  }
+  else if ( ISOInput == "HNL"){
+    Exchange = inputUSD * 24.58;
+  }
+  else if (ISOInput == "HRK"){
+    Exchange = inputUSD * 7.04;
+  }
+  else if (ISOInput == "HTG"){
+    Exchange = inputUSD * 132.05;
+  }
+  else if (ISOInput == "HUF"){
+    Exchange = inputUSD * 347.38;
+  }
+  else if (ISOInput == "IDR"){
+    Exchange = inputUSD * 15,424.30;  //b.c
+  }
+  else if (ISOInput == "ILS"){
+    Exchange = inputUSD * 3.72;
+  }
+  else if (ISOInput == "IMP"){
+    Exchange = inputUSD * 0.806;
+  }
+  else if (ISOInput == "INR"){
+    Exchange = inputUSD * 83.29;
+  }
+  else if (ISOInput == "IQD"){
+    Exchange = inputUSD * 1303.63;
+  }
+  else if (ISOInput == "IRR"){
+    Exchange = inputUSD * 42,262.50;
+  }
+  else if (ISOInput == "ISK"){
+    Exchange = inputUSD * 140.06;
+  }
+  else if (ISOInput == "JEP"){
+    Exchange = inputUSD * 0.84; //flunctuates
+  }
+  else if (ISOInput == "JMD"){
+    Exchange = inputUSD * 155.07;
+  }
+  else if (ISOInput == "JOD"){
+    Exchange = inputUSD * 0.71;
+  }
+  else if (ISOInput == "JPY"){
+    Exchange = inputUSD * 149.54;
+  }
+  else if (ISOInput == "KES"){
+    Exchange = inputUSD * 151.55;
+  }
+  else if (ISOInput == "KGS"){
+    Exchange = inputUSD * 89.13;
+  }
+  else if (ISOInput == "KHR"){
+    Exchange = inputUSD * 4100;
+  }
+  else if (ISOInput == "KID"){
+    Exchange = inputUSD * 0.92;
+  }
+  else if (ISOInput == "KMF"){
+    Exchange = inputUSD * 453.62;
+  }
+  else if (ISOInput == "KRW"){
+    Exchange = inputUSD * 1296.38;
+  }
+  else if (ISOInput == "KWD"){
+    Exchange = inputUSD * 0.31;
+  }
+  else if (ISOInput == "KYD"){
+    Exchange = inputUSD * 0.83;
+  }
+  else if (ISOInput == "KZT"){
+    Exchange = inputUSD * 461.73;
+  }
+  else if (ISOInput == "LAK"){
+    Exchange = inputUSD * 20635.56; //b.c
+  }
+  else if (ISOInput == "LBP"){
+    Exchange = inputUSD * 14966.73;  //b.c
+  }
+  else if (ISOInput == "LKR"){
+    Exchange = inputUSD * 326.43;
+  }
+  else if (ISOInput == "LRD"){
+    Exchange = inputUSD * 187.85;
+  }
+  else if (ISOInput == "LSL"){
+    Exchange = inputUSD * 18.37;
+  }
+  else if (ISOInput == "LYD"){
+    Exchange = inputUSD * 4.82;
+  }
+  else if (ISOInput == "MAD"){
+    Exchange = inputUSD * 10.09;
+  }
+  else if (ISOInput == "MDL"){
+    Exchange = inputUSD * 17.75;
+  }
+  else if (ISOInput == "MGA"){
+    Exchange = inputUSD * 4507.53;
+  }
+  else if (ISOInput == "MKD"){
+    Exchange = inputUSD * 56.43;
+  }
+
+    // cout << "TEST CASE\n" <<endl;
+    // cout << " 1 USD to " << ISOInput << " is " << Exchange << " in ISOInput" <<endl;
+    // cout << " :D" <<endl;
+
+
+
+    return 0;
+}
